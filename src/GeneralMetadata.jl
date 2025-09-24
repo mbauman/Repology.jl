dcee174a96a845f91a0de1fbaef10ee6338831fe
Matt Bauman <mbauman@juliacomputing.com>
2025-09-24 14:32:02 -0400

    ad-hoc licensing stuff

--- src/GeneralMetadata.jl
+++ src/GeneralMetadata.jl
@@ -1,5 +1,21 @@
 module GeneralMetadata
 
+import TOML, JSON3, HTTP, CSV
+using DataFrames: DataFrames, DataFrame
+
+function manifest_packages(manifest_path)
+    collect(keys(TOML.parsefile(manifest_path)["deps"]))
+end
+
 # Write your package code here.
+function license(packagename)
+    JSON3.read(String(HTTP.get("https://juliahub.com/docs/General/$packagename/stable/pkg.json").body)).license
+end
+
+function write_csv_for_manifest(manifest_path, output_path)
+    pkgs = manifest_packages(manifest_path)
+    licenses = [(try license(pkg); catch ex; missing end) for pkg in pkgs]
+    CSV.write(output_path, DataFrame(pkg=pkgs, license=licenses))
+end
 
 end
