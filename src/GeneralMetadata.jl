a7029f7bfd9226e41a09781634c9bbaa3edd9574
Matt Bauman <mbauman@juliahub.com>
2025-10-09 10:25:48 -0400

    fix "not our ref" errors
    this is why imagemagick is not getting the correct version

--- src/GeneralMetadata.jl
+++ src/GeneralMetadata.jl
@@ -119,4 +119,71 @@ function process_commit!(dates, commit)
     return fastpath
 end
 
+function get_version_from_commit(repo, commit; git_cache=Dict{String,String}())
+    dir = get!(git_cache, repo) do
+        tmp = mktempdir()
+        run(pipeline(`git clone --filter=tree:0 --no-checkout --tags $repo $tmp`, stdout=Base.devnull, stderr=Base.devnull))
+        tmp
+    end
+    tag = cd(dir) do
+        try
+            readchomp(`git tag --points-at $commit`)
+        catch _
+            try
+                run(`git fetch origin $commit`)
+                readchomp(`git tag --points-at $commit`)
+            catch _
+                ""
+            end
+        end
+    end
+    # It can be challenging to parse a version number out of a tag; some options here include: v1.2.3 and PCRE2-1.2.3
+    # This strips all non-numeric prefixes with up to one digit as long as the digit is not followed by a period.
+    # and ignore everything after a newline (multiple tags are newline separated, with the latest first)
+    ver = strip(split(chopprefix(tag, r"^[^\d]*(?:\d[^\d.]+)?"), "\n", limit=2)[1])
+    return ver
+end
+
+function add_components!(component_info, source; repositories, url_patterns, git_cache=Dict{String,String}())
+    if haskey(source, "url")
+        for (upstream_project, upstream_version) in all_matches(url_patterns, source["url"])
+            haskey(component_info, upstream_project) ?
+                union!(component_info[upstream_project], [upstream_version]) :
+                component_info[upstream_project] = [upstream_version]
+        end
+    end
+    if haskey(source, "repo") && haskey(source, "hash") && haskey(repositories, source["repo"])
+        upstream_project = repositories[source["repo"]]
+        commit = source["hash"]
+        # Now the hard part are versions...
+        ver = get_version_from_commit(source["repo"], commit; git_cache)
+        if isempty(ver)
+            # try getting it from the prior version
+            # lots of projects tag and then make some minor version number fix
+            ver = get_version_from_commit(source["repo"], commit*"~"; git_cache)
+            !isempty(ver) && @info "$upstream_project: found tag at $commit~"
+        end
+        if !isempty(ver)
+            @info "$upstream_project: got version $(ver) from git repo tag"
+            haskey(component_info, upstream_project) ?
+                union!(component_info[upstream_project], [ver]) :
+                component_info[upstream_project] = [ver]
+        else
+            @info "$upstream_project: failed to get tag from repo $(source["repo"])"
+            component_info[upstream_project] = ["*"]
+        end
+    end
+    return component_info
+end
+
+function all_matches(pattern_project_pairs, needle)
+    result = Tuple{String,String}[]
+    for (pattern, project) in pattern_project_pairs
+        m = match(pattern, needle)
+        !isnothing(m) && push!(result, (project, m.captures[1]))
+    end
+    return result
+end
+
+
 end
